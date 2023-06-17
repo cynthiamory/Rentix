71b72ae0a24781fa97eab846f25902cdda4f15d2
Rohit Nayyar <Rohitnayyar54@gmail.com>
2023-06-17 00:24:48 -0400

    initialized Signup/login made catering and fetch cars Api

--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,56 @@
-// const loginFormHandler = async (event) => {
-//   event.preventDefault();
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  console.log("hello world")
+  // Collect values from the login form
+  const email = document.getElementById('email-login').value.trim();
+  const password = document.getElementById('password-login').value.trim();
 
-//   // Collect values from the login form
-//   const email = document.querySelector('#email-login').value.trim();
-//   const password = document.querySelector('#password-login').value.trim();
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/Login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-//   if (email && password) {
-//     // Send a POST request to the API endpoint
-//     const response = await fetch('/api/users/login', {
-//       method: 'POST',
-//       body: JSON.stringify({ email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
 
-//     if (response.ok) {
-//       // If successful, redirect the browser to the profile page
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
+const signupFormHandler = async (event) => {
+  event.preventDefault();
 
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
+  const name = document.getElementById('name-signup').value.trim();
+  console.log(name)
+  const email = document.getElementById('email-signup').value.trim();
+  console.log(email)
+  const password = document.getElementById('password-signup').value.trim();
+  console.log(password)
 
-//   const name = document.querySelector('#name-signup').value.trim();
-//   const email = document.querySelector('#email-signup').value.trim();
-//   const password = document.querySelector('#password-signup').value.trim();
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-//   if (name && email && password) {
-//     const response = await fetch('/api/users', {
-//       method: 'POST',
-//       body: JSON.stringify({ name, email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
 
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
+document
+  .getElementById('login-form')
+  .addEventListener('click', loginFormHandler);
 
-// document
-//   .querySelector('.login-form')
-//   .addEventListener('submit', loginFormHandler);
-
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
+document
+  .getElementById('signup-form')
+  .addEventListener('click', signupFormHandler);
