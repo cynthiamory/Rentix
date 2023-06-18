5a77ca58e7ac40fb2b091dc1b43a584a185bec04
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-06-19 01:02:56 +0530

    fixed error for /post for acccmodation

--- models/accomodation.js
+++ models/accomodation.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Accomodation extends Model {}
 
@@ -11,50 +11,49 @@ Accomodation.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
-      accommodation_place: {
+    accommodation_place: {
+      type: DataTypes.STRING,
+      defaultValue:"London",
+      allowNull:false,
+    },
+    accommodation_description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { args: true,
+           msg: "You must enter a name"}
+    },},
+    accommodation_location: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    accommodation_description:
-    {
-         type: DataTypes.STRING,
-         allowNull: false,
-     }, 
-     accommodation_location:
-    {
-         type: DataTypes.STRING,
-         allowNull: false,
-     }, 
     price: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-    accommodation_filename:{
+    accommodation_filename: {
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull: false,
     },
-    
+
     trending_id: {
       type: DataTypes.INTEGER,
       // references: {
       //     model: 'trending',
       //     key: 'id',
       // },
+    },
   },
-},
-   
-  
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'accomodation',
+    modelName: "accomodation",
   }
-
 );
 
 //EXPORT
